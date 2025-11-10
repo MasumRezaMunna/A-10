83b3b4195aea2c7b6c564f680d0d5273946216a3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 10:29:09 +0600

    added allmovies page

---
+++ src/pages/AllMovies/AllMovies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllMovies = () => {
+    return (
+        <div className='min-h-screen'>
+            <h1 className='text-3xl font-bold text-center'>All Movies Page</h1>
+        </div>
+    );
+};
+
+export default AllMovies;
