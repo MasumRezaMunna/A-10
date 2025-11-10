c08721e331d9828e3fcf0040f9d2c7d8629bae5e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 10:36:34 +0600

    added register page

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold text-center">Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;
