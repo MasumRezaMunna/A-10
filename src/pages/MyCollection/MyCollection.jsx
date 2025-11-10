06d251fdafbfb938180bf93015c0e835469d4093
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 10:32:07 +0600

    added mycollection page

---
+++ src/pages/MyCollection/MyCollection.jsx
@@ -0,0 +1,9 @@
+const MyCollection = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold text-center">My Collection Page</h1>
+    </div>
+  );
+};
+
+export default MyCollection;
