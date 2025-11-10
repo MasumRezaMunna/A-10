df29f87994f474d135c29c39bfad045f4c3f1cf0
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 10:34:44 +0600

    added login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold text-center">Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
