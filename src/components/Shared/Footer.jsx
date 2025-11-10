9fd60cdf2064788c2388b22df3951283a568bc4e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 22:18:41 +0600

    added firebase

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className="footer p-10 bg-base-200 text-base-content flex justify-between">
       <aside>
         <Link to="/" className="text-2xl font-bold">
           MovieMaster Pro
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdbwzJGIkUvhn39Cfq_l02ZPDu9G5tg4w",
+  authDomain: "moviemaster-pro-b6434.firebaseapp.com",
+  projectId: "moviemaster-pro-b6434",
+  storageBucket: "moviemaster-pro-b6434.firebasestorage.app",
+  messagingSenderId: "297613559261",
+  appId: "1:297613559261:web:0b45f346596db60b72a234"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
