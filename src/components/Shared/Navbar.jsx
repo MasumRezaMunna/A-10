93d7375f98d4ea2076698c9c5689dcf0155e0e14
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-10 10:50:58 +0600

    added navbar page

---
+++ src/components/Shared/Navbar.jsx
@@ -0,0 +1,71 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  
+
+  const user = null; 
+
+  const navLinks = (
+    <>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/movies">All Movies</NavLink></li>
+      {user && (
+        <li><NavLink to="/movies/my-collection">My Collection</NavLink></li>
+      )}
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-md">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52">
+            {navLinks}
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl font-bold">
+          MovieMaster Pro
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navLinks}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        
+
+
+        {user ? (
+          <div className="dropdown dropdown-end z-[10]">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+
+
+
+                
+                <img alt="User profile" src="https://via.placeholder.com/150" />
+              </div>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a className="justify-between">Profile</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        ) : (
+          <>
+            <Link to="/login" className="btn btn-primary btn-sm mr-2">Login</Link>
+            <Link to="/register" className="btn btn-secondary btn-sm">Register</Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
